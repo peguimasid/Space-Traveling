77e4642234788fac593fd3e5786da163747bb031
Guilhermo Masid <guilhermomasid@gmail.com>
2022-03-02 14:56:05 -0300

    chore: get post data by slug form prismic

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -28,26 +28,33 @@ interface PostProps {
   post: Post;
 }
 
-const Post: FunctionComponent = () => {
-  const router = useRouter();
-
-  console.log(router);
-
-  return <h1>Post page</h1>;
+const Post: FunctionComponent = ({ slug }) => {
+  return <h1>{slug}</h1>;
 };
 
 export default Post;
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export const getStaticPaths: GetStaticPaths = async () => {
+  // const prismic = getPrismicClient();
+  // const posts = await prismic.query(TODO);
 
-//   // TODO
-// };
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+};
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+export const getStaticProps: GetStaticProps = async ({ params: { slug } }) => {
+  const prismic = getPrismicClient();
 
-//   // TODO
-// };
+  const response = await prismic.getByUID('posts', String(slug), {});
+
+  console.log(response);
+
+  return {
+    props: {
+      slug,
+    },
+    revalidate: 60 * 60, // 1 hour
+  };
+};
